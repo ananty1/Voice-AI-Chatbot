1bb2016ffb87517fd01b378705d4d7b874a6c2fc
anant <ananty@iitbhilai.ac.in>
2024-09-16 02:10:00 +0530

    Added the page please

--- backend/main.py
+++ backend/main.py
@@ -18,11 +18,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"]
+    allow_headers=["*"]
 )
 @app.get("/")
 async def read_root():
